1622670af6fa451b97c87d5f87b94bbf93654b30
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-10 10:24:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(n)
+  i = 2
+
+  if (n <= 1)
+    return false
+  end
+  while i < n
+      if n % i == 0
+        return false
+      end
+      i += 1
+  end
+  return true
+end
